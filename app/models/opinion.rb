265822445f988f01d4e4af616cadee9992fb205e
Robert Loterh <rloterh@gmail.com>
2021-01-13 16:22:52 +0000

    Create association between User and Opinion

--- app/models/opinion.rb
+++ app/models/opinion.rb
@@ -1,2 +1,4 @@
 class Opinion < ApplicationRecord
+  belongs_to :Author, foreign_key: 'authorId', class_name: 'User'
+  validates :text, presence: true, length: { minimum: 3, maximum: 300 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,3 @@
 class User < ApplicationRecord
+  has_many :opinions, foreign_key: 'authorId', class_name: 'Opinion'
 end
