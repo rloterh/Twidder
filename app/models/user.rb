7e1f5716919d88ccd5ca4e8aeaf7d45fb7c35b8a
Robert Loterh <rloterh@gmail.com>
2021-01-15 22:41:33 +0000

    Add validations to user fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
   has_many :opinions, foreign_key: 'authorId', class_name: 'Opinion'
+
+  before_save { self.username = self.username.downcase }
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 45 }
+  validates :fullname, presence: true, length: { minimum: 3, maximum: 80 }
+
+  def self.all_users(user_id)
+    User.where('id != ?', user_id)
+  end
 end
