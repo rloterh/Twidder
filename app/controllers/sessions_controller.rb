3d2d5f8ec2830753d1ccf09adf33b8dcb8be4e16
Robert Loterh <rloterh@gmail.com>
2021-01-16 08:20:38 +0000

    Update session actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,21 @@
 class SessionsController < ApplicationController
+  def new; end
+
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user
+      session[:user_id] = user.id
+      flash[:notice] = 'Logged in successfully'
+      redirect_to root_path(user)
+    else
+      flash.now[:alert] = 'Incorrect Login information'
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = 'Logged out'
+    redirect_to root_path
+  end
 end
