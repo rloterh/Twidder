ee83a521cc7b4012ef86022db45bd17bd8786142
Robert Loterh <rloterh@gmail.com>
2021-01-16 17:12:12 +0000

    Migrate database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_13_082510) do
+ActiveRecord::Schema.define(version: 2021_01_14_114142) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,9 @@ ActiveRecord::Schema.define(version: 2021_01_13_082510) do
     t.text "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "pic"
+    t.text "coverImg"
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
