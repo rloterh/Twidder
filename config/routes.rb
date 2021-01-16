490f4fa7ea50960e1bc93fbc315044bda9d6c1dd
Robert Loterh <rloterh@gmail.com>
2021-01-16 17:14:53 +0000

    Update application routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  resources :opinions
-  resources :users
+ # resources :opinions
+  resources :users,  except: [:new]
   get '/', to: 'opinions#index', as: :root
+  get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  post '/create_opinion', to: 'opinions#create', as: :new_opinion
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
