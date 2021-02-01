fced0cbe7d84198eec3313c9477018f84b11f500
Robert Loterh <rloterh@gmail.com>
2021-02-01 07:57:24 +0000

    Define Following associations with User

--- app/models/following.rb
+++ app/models/following.rb
@@ -1,2 +1,12 @@
 class Following < ApplicationRecord
+  belongs_to :Follower, class_name: 'User'
+  belongs_to :Followed, class_name: 'User'
+
+  validates :follower_id, presence: true
+  validates :followed_id, presence: true
+
+  def self.unfollow(sender_id, receiver_id)
+    Following.where(follower_id: sender_id).where(followed_id: receiver_id)[0]
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,18 @@
 class User < ApplicationRecord
   has_many :opinions, foreign_key: 'author_id', class_name: 'Opinion'
 
+  has_many :followed, foreign_key: 'follower_id', class_name: 'Following'
+  has_many :followers, foreign_key: 'followed_id', class_name: 'Following'
+
   before_save { self.username = self.username.downcase }
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 45 }
   validates :fullname, presence: true, length: { minimum: 3, maximum: 80 }
 
   def self.all_users(user_id)
     User.where('id != ?', user_id)
+  end
+
+  def self.followers(id, curr_user_id)
+    Following.where(followed_id: id).where.not(follower_id: curr_user_id).order(created_at: :desc).limit(5)
   end
 end
