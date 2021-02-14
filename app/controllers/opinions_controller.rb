647b86d7bff086547a17d9ee4533c3eb1d93d210
Robert Loterh <rloterh@gmail.com>
2021-02-14 17:21:03 +0000

    Modify Opinion actions

--- app/controllers/opinions_controller.rb
+++ app/controllers/opinions_controller.rb
@@ -1,6 +1,10 @@
 class OpinionsController < ApplicationController
   before_action :verify, only: [:index]
 
+  def new
+    @opinion = current_user.opinions.build
+  end
+
   def index
     @opinion = Opinion.new
     @opinions = Opinion.order('created_at DESC').includes(:Author).limit(5)
@@ -10,20 +14,21 @@ class OpinionsController < ApplicationController
   def create
     @opinion = Opinion.new(opinion_params)
     @opinion.author_id = current_user.id
-
-    if @opinion.save
-      flash[:notice] = 'Opinion was successfully created'
-      redirect_to root_path
-    else
-      flash[:alert] = 'Something went wrong'
-      render 'index'
+    respond_to do |format|
+      if @opinion.save
+        format.html { redirect_to root_path }
+        format.json { render root_path, status: :created, location: @opinion }
+      else
+        flash[:alert] = @opinion.errors.full_messages.join('. ').to_s
+        format.html { redirect_to root_path }
+        format.json { render json: @opinion.errors, status: :unprocessable_entity }
+      end
     end
   end
 
   private
-
-    # Only allow a list of trusted parameters through.
-    def opinion_params
-      params.require(:opinion).permit(:text)
-    end
+  # Only allow a list of trusted parameters through.
+  def opinion_params
+    params.require(:opinion).permit(:text)
+  end
 end
