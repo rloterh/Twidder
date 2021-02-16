c49fc42a824b3fc62dcd8431d4c6e113f1138b6c
Robert Loterh <rloterh@gmail.com>
2021-02-16 08:23:14 +0000

    Modify Following model query and associations

--- app/models/following.rb
+++ app/models/following.rb
@@ -1,6 +1,6 @@
 class Following < ApplicationRecord
-  belongs_to :Follower, class_name: 'User'
-  belongs_to :Followed, class_name: 'User'
+  belongs_to :Follower, foreign_key: 'follower_id', class_name: 'User'
+  belongs_to :Followed, foreign_key: 'followed_id', class_name: 'User'
 
   validates :follower_id, presence: true
   validates :followed_id, presence: true
@@ -8,5 +8,4 @@ class Following < ApplicationRecord
   def self.unfollow(sender_id, receiver_id)
     Following.where(follower_id: sender_id).where(followed_id: receiver_id)[0]
   end
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,14 +5,14 @@ class User < ApplicationRecord
   has_many :followers, foreign_key: 'followed_id', class_name: 'Following'
 
   before_save { self.username = self.username.downcase }
-  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 45 }
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 50 }
   validates :fullname, presence: true, length: { minimum: 3, maximum: 80 }
+
+  def self.user_followers(id, curr_user_id)
+    Following.where(followed_id: id).where.not(follower_id: curr_user_id).order(created_at: :desc).limit(5)
+  end
 
   def self.all_users(user_id)
     User.where('id != ?', user_id)
-  end
-
-  def self.followers(id, curr_user_id)
-    Following.where(followed_id: id).where.not(follower_id: curr_user_id).order(created_at: :desc).limit(5)
   end
 end
