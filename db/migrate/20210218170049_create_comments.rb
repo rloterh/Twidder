cb1af09af32fe63a521886fa8fdb8f93d106c28c
Robert Loterh <rloterh@gmail.com>
2021-02-18 18:56:28 +0000

    Update Comment schema

--- db/migrate/20210218170049_create_comments.rb
+++ db/migrate/20210218170049_create_comments.rb
@@ -1,11 +1,13 @@
 class CreateComments < ActiveRecord::Migration[6.1]
   def change
     create_table :comments do |t|
-      t.integer :author_id
+      t.integer :commentor_id
       t.integer :opinion_id
       t.text :content
 
       t.timestamps
     end
+    add_index :comments, :opinion_id
+    add_foreign_key :comments, :users, column: :commentor_id
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_30_150011) do
+ActiveRecord::Schema.define(version: 2021_02_18_170049) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "commentor_id"
+    t.integer "opinion_id"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["opinion_id"], name: "index_comments_on_opinion_id"
+  end
 
   create_table "followings", force: :cascade do |t|
     t.integer "follower_id"
@@ -42,4 +51,5 @@ ActiveRecord::Schema.define(version: 2021_01_30_150011) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "comments", "users", column: "commentor_id"
 end
