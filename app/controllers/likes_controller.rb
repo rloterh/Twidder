c30b15d52ba634ea5b61204fc9d62c779e95e7bf
Robert Loterh <rloterh@gmail.com>
2021-02-26 08:31:35 +0000

    Update Like actions

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,74 +1,29 @@
 class LikesController < ApplicationController
-  before_action :set_like, only: [:show, :edit, :update, :destroy]
-
-  # GET /likes
-  # GET /likes.json
-  def index
-    @likes = Like.all
-  end
-
-  # GET /likes/1
-  # GET /likes/1.json
-  def show
-  end
-
-  # GET /likes/new
-  def new
-    @like = Like.new
-  end
-
-  # GET /likes/1/edit
-  def edit
-  end
-
-  # POST /likes
-  # POST /likes.json
   def create
-    @like = Like.new(like_params)
+    @like = current_user.likes.new(opinion_id: params[:opinion_id])
 
     respond_to do |format|
       if @like.save
-        format.html { redirect_to @like, notice: 'Like was successfully created.' }
-        format.json { render :show, status: :created, location: @like }
+        format.html { redirect_to opinions_path, notice: 'You liked a twid.' }
+        format.json { render opinions_path, status: :created, location: @opinions }
       else
-        format.html { render :new }
+        format.html { redirect_to root_path, alert: 'You cannot like this twid.' }
         format.json { render json: @like.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /likes/1
-  # PATCH/PUT /likes/1.json
-  def update
-    respond_to do |format|
-      if @like.update(like_params)
-        format.html { redirect_to @like, notice: 'Like was successfully updated.' }
-        format.json { render :show, status: :ok, location: @like }
-      else
-        format.html { render :edit }
-        format.json { render json: @like.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /likes/1
-  # DELETE /likes/1.json
   def destroy
-    @like.destroy
+    like = Like.find_by(id: params[:id], user: current_user, opinion_id: params[:opinion_id]) 
     respond_to do |format|
-      format.html { redirect_to likes_url, notice: 'Like was successfully destroyed.' }
-      format.json { head :no_content }
+      if like
+        like.destroy
+        format.html { redirect_to opinions_path, notice: 'You disliked a twid.' }
+        format.json { head :no_content }
+      else
+        format.html { redirect_to opinions_path, alert: 'You cannot dislike twid that you did not like before.' }
+        format.json { render json: @like.errors, status: :unprocessable_entity }
+      end
     end
   end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_like
-      @like = Like.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def like_params
-      params.require(:like).permit(:user_id, :opinion_id)
-    end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,13 @@ module ApplicationHelper
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.username, class: 'user-img')
   end
+
+ def like_or_dislike_btn(opinion)
+    like = Like.find_by(opinion: opinion, user: current_user)
+    if like
+      link_to('Dislike!', opinion_like_path(id: like.id, opinion_id: opinion.id), method: :delete)
+    else
+      link_to('Like!', opinion_likes_path(opinion_id: opinion.id), method: :post)
+    end
+  end
 end
