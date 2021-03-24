8956b401b43a4c646985ff579d02cb2afdc14227
Robert Loterh <rloterh@gmail.com>
2021-03-24 16:43:31 +0000

    Modify database configuration

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Twidder
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.assets.initialize_on_precompile = false
 
     # Configuration for the application, engines, and railties goes here.
     #
